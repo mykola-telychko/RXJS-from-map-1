840c7a50270fa4700c4d3c28cef84e147107c0b1
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 20:53:05 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { from } from 'rxjs';
 
-//works on js collections
+// https://www.learnrxjs.io/learn-rxjs/operators/creation/from
+// Example 3: Observable from collection
+// works on js collections
 const map = new Map();
 map.set(1, 'Hi');
 map.set(2, 'Bye');
